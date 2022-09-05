b44abcf7879f954b0e54c6d3be679dea7b38a7e9
Jesus Angulo <jesus.angulo@outlook.com>
2022-09-05 22:37:18 +0000

    Cleaning up demo

--- DBload.js
+++ DBload.js
@@ -1,13 +1,14 @@
-const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const Load = require('./src/DBload/Load.js')
 
-
 conn.sync({ force: true })
-.then(async()=>await Load.loadTypes())
-.then(async()=>{
-  for(let i=0; i<58;i++){
-    await Load.loadPokemons(i*20,20);
-  }
-})
-.then(()=>console.log('listo'));
+  .then(async () => await Load.loadTypes())
+  .then(async () => {
+    for (let i = 0; i < 58; i++) {
+      const take = 20;
+      const resumeIndex = i * take;
+      await Load.loadPokemons(i * 20, 20);
+      console.info(`Recorded from ${resumeIndex}...`);
+    }
+  })
+  .then(() => console.log('listo'));
--- src/db.js
+++ src/db.js
@@ -3,10 +3,10 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST,
+  DB_USER, DB_PASSWORD, DB_HOST, DB_NAME
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pokedb`, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
@@ -35,8 +35,8 @@ const { Pokemon, Types } = sequelize.models;
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
 
-Pokemon.belongsToMany(Types, {through: 'poke-types'});
-Types.belongsToMany(Pokemon, {through: 'poke-types'});
+Pokemon.belongsToMany(Types, { through: 'poke-types' });
+Types.belongsToMany(Pokemon, { through: 'poke-types' });
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
