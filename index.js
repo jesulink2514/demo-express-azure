970fd72d6879fdce29cca5546f0f07062a6ccba5
Jesus Angulo <jesus.angulo@outlook.com>
2022-09-05 17:19:22 -0500

    Cleaning up

--- index.js
+++ index.js
@@ -1,38 +1,9 @@
-//                       _oo0oo_
-//                      o8888888o
-//                      88" . "88
-//                      (| -_- |)
-//                      0\  =  /0
-//                    ___/`---'\___
-//                  .' \\|     |// '.
-//                 / \\|||  :  |||// \
-//                / _||||| -:- |||||- \
-//               |   | \\\  -  /// |   |
-//               | \_|  ''\---/''  |_/ |
-//               \  .-\__  '-'  ___/-. /
-//             ___'. .'  /--.--\  `. .'___
-//          ."" '<  `.___\_<|>_/___.' >' "".
-//         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
-//         \  \ `_.   \_ __\ /__ _/   .-` /  /
-//     =====`-.____`.___ \_____/___.-`___.-'=====
-//                       `=---='
-//     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const Load = require('./src/DBload/Load.js')
 
 const port = process.env.PORT || 3001;
 
-// Syncing all the models at once.
-// conn.sync({ force: true })
-// .then(async()=>await Load.loadTypes())
-// .then(async()=>{
-//   for(let i=0; i<58;i++){
-//     await Load.loadPokemons(i*20,20);
-//   }
-// })
-// .then(() => {
-  server.listen(port, () => {
-    console.log(`%s listening at ${port}`); // eslint-disable-line no-console
-  });
-// })
+server.listen(port, () => {
+  console.log(`%s listening at ${port}`); // eslint-disable-line no-console
+});
